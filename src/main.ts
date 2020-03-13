711fe7619d4fcaaecb240969bd6dd83c241d8ccb
Dieter 💤 <dieter.vanbroeck@ordina.be>
2020-03-13 10:28:06 +0100

    Load styles, bootstrap, fontawesome and compostion api

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import VueCompositionApi from '@vue/composition-api';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './styles.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
 
 new Vue({
     router,
