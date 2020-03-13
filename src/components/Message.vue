be7c0c04716328e780d6fdbd66fada607af69de5
Dieter 💤 <dieter.vanbroeck@ordina.be>
2020-03-13 10:35:41 +0100

    Message with props

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,11 @@
 <template>
     <div data-component="message" class="message-container row">
-        <div class="arrow-left"></div>
+        <div v-if="messageType === MessageType.Other" class="arrow-left"></div>
         <div class="alert alert-info col-md-8">
-            <div class="user text-muted small">(20:12) Didii says:</div>
-            <div class="message content">This is my message!</div>
+            <div class="user text-muted small">({{hour}}:{{minute}}) {{userName}} says:</div>
+            <div class="message content">{{message}}</div>
         </div>
-        <div class="arrow-right"></div>
+        <div v-if="messageType === MessageType.Own" class="arrow-right"></div>
     </div>
 </template>
 
@@ -15,7 +15,27 @@ import { MessageType } from '@/models/message-type';
 
 @Component
 export default class Message extends Vue {
+    @Prop({default: MessageType.Own}) public messageType!: MessageType;
+    @Prop({default: 'N/A'}) public userName!: string;
+    @Prop({default: () => new Date()}) public timeStamp!: Date;
+    @Prop({default: 'Message could not be loaded...'}) public message!: string;
 
+    private get hour(): string {
+        const hour = this.timeStamp.getHours();
+        if (hour < 10) {
+            return '0' + hour;
+        }
+        return '' + hour;
+    }
+    private get minute(): string {
+        const minute = this.timeStamp.getMinutes();
+        if (minute < 10) {
+            return '0' + minute;
+        }
+        return '' + minute;
+    }
+
+    private MessageType = MessageType;
 }
 </script>
 
